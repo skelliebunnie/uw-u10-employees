960321ba0db1d5690a5fb1e46dc1c8f887e24563
Angel <skelliebunnie@gmail.com>
2021-01-12 16:30:35 -0800

    init and add functions created

--- app.js
+++ app.js
@@ -9,10 +9,19 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+let manager = null;
+
+function init() {
+	if(manager === null) {
+		addManager();
+
+	} else {
+		addEmployees();
+
+	}
+}
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -33,3 +42,142 @@ const render = require("./lib/htmlRenderer");
 // for further information. Be sure to test out each class and verify it generates an
 // object with the correct structure and methods. This structure will be crucial in order
 // for the provided `render` function to work! ```
+
+function addManager() {
+	inquirer.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "Manager's Name: "
+		},
+		{
+			type: "input",
+			name: "email",
+			message: "Manager's Email: "
+		},
+		{
+			type: "number",
+			name: "id",
+			message: "Manager's ID: "
+		},
+		{
+			type: "input",
+			name: "github",
+			message: "Manager's Office Number: "
+		}
+	])
+	.then(answers => {
+		manager = answers;
+
+		console.log(manager);
+		addEmployees();
+	})
+	.catch(err => {
+		console.error(error);
+	});
+}
+
+function addEmployees() {
+	inquirer.prompt([
+			{
+				type: "list",
+				message: "What would you like to do?",
+				name: "action",
+				choices: [
+					{
+						name: "add Engineer",
+						value: "addEngineer"
+					},
+					{
+						name: "add Intern",
+						value: "addIntern"
+					},
+					{
+						name: "Exit",
+						value: "exit"
+					}
+				]
+			}
+		])
+		.then(answers => {
+			if(answers.action === "addEngineer") {
+				addEngineer();
+			}
+
+			if(answers.action === "addIntern") {
+				addIntern();
+			}
+
+			if(answers.action === "exit") {
+				console.log("render will be called here");
+			}
+
+		})
+		.catch(error => {
+			console.error(error);
+		})
+}
+
+function addEngineer() {
+	inquirer.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "Engineer's Name: "
+		},
+		{
+			type: "input",
+			name: "email",
+			message: "Engineer's Email: "
+		},
+		{
+			type: "number",
+			name: "id",
+			message: "Engineer's ID: "
+		},
+		{
+			type: "input",
+			name: "github",
+			message: "Engineer's GitHub Username: "
+		}
+	])
+	.then(answers => {
+		console.log(answers);
+	})
+	.catch(err => {
+		console.error(error);
+	});
+}
+
+function addIntern() {
+	inquirer.prompt([
+		{
+			type: "input",
+			name: "name",
+			message: "Intern's Name: "
+		},
+		{
+			type: "input",
+			name: "email",
+			message: "Intern's Email: "
+		},
+		{
+			type: "number",
+			name: "id",
+			message: "Intern's ID: "
+		},
+		{
+			type: "input",
+			name: "github",
+			message: "Intern's School: "
+		}
+	])
+	.then(answers => {
+		console.log(answers);
+	})
+	.catch(err => {
+		console.error(error);
+	});
+}
+
+init();
